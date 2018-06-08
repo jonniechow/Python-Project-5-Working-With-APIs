9d80bac1401ee9091066c049a5f51400cd9fe513
Jonny Chow <Jonny@MacBook-Pro-8.local>
2018-06-07 21:09:23 -0400

    Created bar graph to represent the most starred Python projects on GitHub.

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -12,10 +14,17 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
-# Examine the first repository.
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
